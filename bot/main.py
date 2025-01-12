200267a72c200b76a0c439556352f796fcfbe679
Kaz <kazerog.workspace@gmail.com>
2025-01-12 15:21:09 +0300

    save for test

--- bot/main.py
+++ bot/main.py
@@ -6,6 +6,7 @@ import texts
 from aiogram import Bot, Dispatcher
 from aiogram.enums.parse_mode import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.client.default import DefaultBotProperties
 from handlers import router
 from sqlite_db import connect_db
 
@@ -13,7 +14,7 @@ from sqlite_db import connect_db
 async def main():
     connect_db()
 
-    bot = Bot(token=src.BOT_TOKEN, parse_mode=ParseMode.HTML)
+    bot = Bot(token=src.BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
     dp = Dispatcher(storage=MemoryStorage())
     dp.include_router(router)
     await bot.set_my_description(texts.BOT_DESCRIPRION)
